2c922918aca4da3183f53fb42fa4f5613b94a67c
ranajasite <ranajadev@hotmail.com>
2024-07-21 19:29:06 +0100

    Add files via upload

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1721586329|1269540265";
+const CACHE_VERSION = "1721586486|1426518167";
 const CACHE_PREFIX = "Ranaja Therapeut-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
